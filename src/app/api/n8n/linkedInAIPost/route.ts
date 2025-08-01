5b3e755c5fa00e71d68d272ac947e78ef388b265
murali-yenmin <104124109+murali-yenmin@users.noreply.github.com>
2025-08-01 14:42:35 +0530

    code updated for some alignment

--- src/app/api/n8n/linkedInAIPost/route.ts
+++ src/app/api/n8n/linkedInAIPost/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const N8N_WEBHOOK_URL = "https://n8n-k70h.onrender.com/webhook/socialMedia";
+const N8N_WEBHOOK_URL = `${process.env.WEBHOOK_URL}/YenminSocialMedia`;
 
 export async function POST(req: NextRequest) {
   try {
